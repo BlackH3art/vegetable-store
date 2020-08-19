dee45b9a591a9093fa42e778cc9b25df5a3a0f77
BlackH3art <jaras2803@o2.pl>
2020-08-19 17:24:46 +0200

    Routing, redux store connected, category nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { VegetableStoreDataStore } from "./data/DataStore";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { ShopConnector } from "./shop/ShopConnector";
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
 
-
-    </div>
-  );
+  render() { 
+    return ( 
+      <Provider store={VegetableStoreDataStore}>
+        <Router>
+          <Switch>
+            <Route path="/shop" component={ShopConnector} />
+            <Redirect to="/shop" />
+          </Switch>
+        </Router>
+      </Provider>
+     );
+  }
 }
+ 
 
-export default App;
+
+
